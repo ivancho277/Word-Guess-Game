eceaf381639059506f4b35e8815ad444a8182820
ivancho277 <rouskoi@live.com>
2019-03-01 06:07:46 -0800

    much of game logic working, still some main parts left, all console logging correctly so far

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,26 @@
-var wordchoices = ["volkl", "fischer", "line", "armada", "icelantic", "salomon", "rossignol", "elan", "blackdiamond", "atomic", "blizzard", "nordica", "faction", "dynastar", "volant", "head", "moment", "stokli", "scott"];
+var wordchoices = [
+  'volkl',
+  'fischer',
+  'line',
+  'armada',
+  'icelantic',
+  'salomon',
+  'rossignol',
+  'elan',
+  'blackdiamond',
+  'atomic',
+  'blizzard',
+  'nordica',
+  'faction',
+  'dynastar',
+  'volant',
+  'head',
+  'moment',
+  'stokli',
+  'scott',
+];
 var guessesremaining = 13;
-var currentword = chooseRandWord(wordchoices);
+var currentword = chooseRandWord (wordchoices);
 // var lettersguessed = [];
 var keepPlaying = true;
 
@@ -9,54 +29,69 @@ var looses = 0;
 var lettersGuessed = [];
 var guessesremaining = 10;
 
-
-
-
-var chosenwordText = document.getElementById("chosen-word");
-var winText = document.getElementById("wins-text");
-var chosenwordblanksText = document.getElementById("chosen-word-blanks-text");
-var InstructionText = document.getElementById("instruction-text");
-var remainingguessesText = document.getElementById("remainingguesses-text");
-var lettersguessedText = document.getElementById("lettersguessed-text");
-var startText = document.getElementById("start-text");
+var chosenwordText = document.getElementById ('chosen-word');
+var winText = document.getElementById ('wins-text');
+var chosenwordblanksText = document.getElementById ('chosen-word-blanks-text');
+var InstructionText = document.getElementById ('instruction-text');
+var remainingguessesText = document.getElementById ('remainingguesses-text');
+var lettersguessedText = document.getElementById ('lettersguessed-text');
+var startText = document.getElementById ('start-text');
 
 //returns randomly chosen word from array of words
 function chooseRandWord (wordarray) {
-    return wordarray[Math.floor(Math.random()*wordarray.length)];
+  return wordarray[Math.floor (Math.random () * wordarray.length)];
 }
 
-//returns string of blank spaces equal to work 
-function printBlank(wordToPrint) {
-    var stringOfBlanks = "";
-    for(var i = 0; i < wordToPrint.length; i++){
-        stringOfBlanks += "_ ";
+//returns string of blank spaces equal to work
+function printBlank (wordToPrint) {
+  var stringOfBlanks = '';
+  for (var i = 0; i < wordToPrint.length; i++) {
+    stringOfBlanks += '-';
+  }
+  return stringOfBlanks;
+}
+
+//takes a guessed letter from user, pushes it into array of guessed chars, subracts 1 from guesses left
+function userGuess (guessedChar) {
+  lettersGuessed.push(guessedChar.toLowerCase ());
+  guessesremaining--;
+}
+
+
+//takes input word, spits out array of its chars
+function wordToCharArray(word){
+    let charArray = [];
+    for(var i = 0; i < word.length; i++){
+        charArray.push(word[i]);
     }
-    return stringOfBlanks;
+    return charArray;
 }
 
-function userGuess(guessedChar){
-    lettersGuessed.push(guessedChar.toLowerCase());
-    guessesremaining--;
+//How to replace blanks??
 
+//check if game is over
+function gameOver (numGuesses) {
+  if (numGuesses > 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-function gameOver(numGuesses){
-    if(numGuesses === 0){
-        document.write("LOSER!");
+
+//Testing
+  document.onkeyup = function (event) {  
+      
+    console.log (currentword);                      //test currentword chosen at random
+    console.log(wordToCharArray(currentword));      //test character array
+    console.log (printBlank (currentword));         //test priting blanks
+    if (event.key !== "F5") {                       //makes sure that refresh key doesnt count as key for testing
+      console.log (event.key);                      //logs pressed key
+      userGuess (event.key);                        //passes key pressed to userGuess function
+      console.log (lettersGuessed.toString ());     //logs users guesses.
+      console.log (guessesremaining);               //logs how many guesses remain
+      chosenwordblanksText.textContent = printBlank (currentword);  //prints blank spaces to html.
+    
     }
-}
+  };
 
-
-
-document.onkeyup = function(event){
-console.log(currentword);
-console.log(printBlank(currentword));
-if(event.key){
-    console.log(event.key);
-    userGuess(event.key);
-    console.log(lettersGuessed.toString())
-    console.log(guessesremaining);
-chosenwordblanksText.textContent = printBlank(currentword);
-gameOver(guessesremaining);
-}
-}
